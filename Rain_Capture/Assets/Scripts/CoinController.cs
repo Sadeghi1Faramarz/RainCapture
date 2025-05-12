50fd1e6ac646e4a4d2ad6560a78f8fb435c0250e
sadeghi1faramarz <regularwebuser@gmail.com>
2025-05-12 20:38:07 +0330

    add surfboard to character

---
+++ Rain_Capture/Assets/Scripts/CoinController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    public float fallSpeed = 3f;
+
+    void Update()
+    {
+        transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+
+        if (transform.position.y < -6f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Coin Catched");
+            Destroy(gameObject);
+        }
+    }
+}
